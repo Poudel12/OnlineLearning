723576c245fff9bad66f0fdbcbab844c921d1aba
Poudel12 <np03cs4a220259@heraldcollege.edu.np>
2025-01-07 13:32:44 +0545

    completed making login and registered form

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,5 +1,53 @@
-export const signUpFoormControls = [
+export const signUpFormControls = [
     {
+        name: 'userName',
+        label : 'User Name',
+        type: 'text',
+        placeholder: 'Enter your user name',
+        componentType: 'input',
+        
+    },
 
-    }
-]
+    {
+        name: 'userEmail',
+        label : 'User Email',
+        type: 'email',
+        placeholder: 'Enter your user email',
+        componentType: 'input'
+    },
+
+    {
+        name: 'password',
+        label : 'User password',
+        type: 'password',
+        placeholder: 'Enter your user password',
+        componentType: 'input'
+    },
+];
+
+export const signInFormControls = [
+  {
+    name: "userEmail",
+    label: "User Email",
+    placeholder: "Enter your user email",
+    type: "email",
+    componentType: "input",
+  },
+  {
+    name: "password",
+    label: "Password",
+    placeholder: "Enter your password",
+    type: "password",
+    componentType: "input",
+  },
+];
+
+export const initialSignInFormData = {
+  userEmail: "",
+  password: "",
+};
+export const initialSignUpFormData = {
+  userName: "",
+  userEmail: "",
+  password: "",
+};
--- frontend/src/pages/auth/index.jsx
+++ frontend/src/pages/auth/index.jsx
@@ -1,23 +1,65 @@
 
 import { TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
 import { GraduationCap } from "lucide-react";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import {Tabs} from "@/components/ui/Tabs";
+import CommonForm from "@/components/common-form";
+import { signUpFormControls, signInFormControls } from "@/config";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { AuthContext } from "@/context/auth-context";
+
+
 
 function AuthPage() {
     const [activeTab, setActiveTab] = useState('login');
+    const {
+    signInFormData,
+    setSignInFormData,
+    signUpFormData,
+    setSignUpFormData,
+    handleRegisterUser,
+    handleLoginUser,
+    } = useContext(AuthContext);
 
     function handleTabChange(value){
-        setActiveTab(value);
+      setActiveTab(value);
     }
+
+    function checkIfSignInFormIsValid() {
+    return (
+      signInFormData &&
+      signInFormData.userEmail !== "" &&
+      signInFormData.password !== ""
+    );
+   }
+
+   function checkIfSignUpFormIsValid() {
+    return (
+      signUpFormData &&
+      signUpFormData.userName !== "" &&
+      signUpFormData.userEmail !== "" &&
+      signUpFormData.password !== ""
+    );
+   }
+
+
+
+
+   console.log(signInFormData);
 
 
     return  <div className="flex flex-col min-h-screen">
         <header className="px-4 lg:px-6 h-14 flex item-center border-b">
             <Link to={'/'} className="flex item-center justify-center">
             <GraduationCap className="h-8 w-8 mr-4"/>
-            <span className="font-extrabold text-xl">My Learning</span>
+            <span className="font-extrabold text-xl">InfiniteLearn</span>
             </Link>
         </header>
         <div className="flex items-center justify-center min-h-screen bg-background">
@@ -50,8 +92,48 @@ function AuthPage() {
                 Register
               </TabsTrigger>
             </TabsList>
-            <TabsContent value='login'> </TabsContent>
-            <TabsContent value='register'> </TabsContent>
+            <TabsContent value='login'>Login 
+             <Card className="p-6 space-y-4">
+              <CardHeader>
+                <CardTitle>Login in to your account</CardTitle>
+                <CardDescription>
+                  Enter your email and password to access your account
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <CommonForm
+                  formControls={signInFormControls}
+                  buttonText={"LogIn"}
+                  formData={signInFormData}
+                  setFormData={setSignInFormData}
+                  isButtonDisabled={!checkIfSignInFormIsValid()}
+                  handleSubmit={handleLoginUser}
+                />
+              </CardContent>
+            </Card>
+
+            </TabsContent>
+            <TabsContent value='register'>register
+              <Card className="p-6 space-y-4">
+              <CardHeader>
+                <CardTitle>Create a new account</CardTitle>
+                <CardDescription>
+                  Enter your details to get started
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <CommonForm
+                  formControls={signUpFormControls}
+                  buttonText={"Register"}
+                  formData={signUpFormData}
+                  setFormData={setSignUpFormData}
+                  isButtonDisabled={!checkIfSignUpFormIsValid()}
+                  handleSubmit={handleRegisterUser}
+                />
+              </CardContent>
+            </Card>
+            
+               </TabsContent>
                     
              </Tabs> 
         </div>
