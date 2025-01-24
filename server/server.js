9e12035bfc7f8a0bbdcf3de8e3db22a4a0cd865d
Poudel12 <np03cs4a220259@heraldcollege.edu.np>
2025-01-24 22:50:07 +0545

    setup cloudinary api and upload routes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const authRoutes = require("./routes/auth-routes/index");
+const mediaRoutes = require("./routes/instructor-routes/media-routes");
 
 
 const app = express();
@@ -29,7 +30,7 @@ mongoose
     .catch((e)=> console.log(e));
 //routes configuration
 app.use("/auth", authRoutes);
-
+app.use("/media", mediaRoutes);
 
 
 
