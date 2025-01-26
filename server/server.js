9a54c7890c28b8c85b992889302f5604a9ff4000
Poudel12 <np03cs4a220259@heraldcollege.edu.np>
2025-01-26 10:22:50 +0545

    make video routes, course controller

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const authRoutes = require("./routes/auth-routes/index");
 const mediaRoutes = require("./routes/instructor-routes/media-routes");
-
+const instructorCourseRoutes = require("./routes/instructor-routes/course-routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000; 
@@ -31,6 +31,7 @@ mongoose
 //routes configuration
 app.use("/auth", authRoutes);
 app.use("/media", mediaRoutes);
+app.use("/instructor/course", instructorCourseRoutes);
 
 
 
