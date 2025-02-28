34e74ffa5e47e2a63b3078b419852690c41360ce
Poudel12 <np03cs4a220259@heraldcollege.edu.np>
2025-02-28 15:33:35 +0545

    set up payment process like ,routes, order schema setup paypal env

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const authRoutes = require("./routes/auth-routes/index");
 const mediaRoutes = require("./routes/instructor-routes/media-routes");
 const instructorCourseRoutes = require("./routes/instructor-routes/course-routes");
 const studentViewCourseRoutes = require("./routes/student-routes/course-routes");
-
+const studentViewOrderRoutes = require("./routes/student-routes/order-routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000; 
@@ -35,6 +35,7 @@ app.use("/auth", authRoutes);
 app.use("/media", mediaRoutes);
 app.use("/instructor/course", instructorCourseRoutes);
 app.use("/student/course", studentViewCourseRoutes);
+app.use("/student/order", studentViewOrderRoutes);
 
 
 
