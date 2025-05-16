845494e59dd24682339735dd5ac1b7f7423825f6
Poudel12 <np03cs4a220259@heraldcollege.edu.np>
2025-05-16 18:52:57 +0545

    Added feedback table

--- frontend/src/pages/admin/Feedbacks.jsx
+++ frontend/src/pages/admin/Feedbacks.jsx
@@ -1,8 +1,17 @@
 import { getAllFeedbackService } from "@/services";
 import { useEffect, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const Feedbacks = () => {
   const [data, setData] = useState([]);
+
   useEffect(() => {
     const getFeedbacks = async () => {
       const res = await getAllFeedbackService();
@@ -11,7 +20,6 @@ const Feedbacks = () => {
     getFeedbacks();
   }, []);
 
-  console.log(data);
   const curriculumData = data?.map((item) => {
     return {
       description: item.description,
@@ -23,8 +31,46 @@ const Feedbacks = () => {
       }),
     };
   });
-  console.log(curriculumData);
-  return <div>Feedbacks</div>;
+
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-6 mb-6">
+      <h1 className="text-xl font-bold mb-2">Feedbacks</h1>
+      <p className="text-gray-600 mb-4">List of all feedbacks.</p>
+      <div className="overflow-x-auto">
+        <Table className="border border-gray-200 rounded-lg overflow-hidden">
+          <TableHeader className="bg-gray-100">
+            <TableRow>
+              <TableHead>SN</TableHead>
+              <TableHead>Student Name</TableHead>
+              <TableHead>Student Email</TableHead>
+              <TableHead>Description</TableHead>
+              <TableHead>Course Title</TableHead>
+              <TableHead>Curriculum Title</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {curriculumData?.map((item, index) => (
+              <TableRow
+                key={index}
+                className="border-b border-gray-200 hover:bg-gray-50 transition-all duration-200"
+              >
+                <TableCell className="p-3">{index + 1}</TableCell>
+                <TableCell className="p-3">{item.studentName}</TableCell>
+                <TableCell className="p-3">{item.studentEmail}</TableCell>
+                <TableCell className="p-3">{item.description}</TableCell>
+                <TableCell className="p-3">{item.courseTitle}</TableCell>
+                <TableCell className="p-3">
+                  {item.curriculum
+                    .map((curriculumItem) => curriculumItem.title)
+                    .join(", ")}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
 };
 
 export default Feedbacks;
